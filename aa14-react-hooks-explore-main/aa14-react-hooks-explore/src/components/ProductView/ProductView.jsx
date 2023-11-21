92b981272a614346ecc1209b785d83543cdaa125
integgy <matthewregidor@gmail.com>
2023-11-20 16:54:05 -0800

    hooks completed

---
+++ aa14-react-hooks-explore-main/aa14-react-hooks-explore/src/components/ProductView/ProductView.jsx
@@ -0,0 +1,49 @@
+import {React, useEffect, useState} from 'react';
+import ProductListItem from '../ProductListItem';
+import ProductDetails from '../ProductDetails';
+import './ProductView.css';
+
+function ProductView({ products }) {
+
+  // TODO: Replace with state variable
+  const [sideOpen, setSideOpen] = useState(true);
+  const [selectedProduct, setSelectedProduct] = useState("")
+
+  useEffect(()=> {
+    if (selectedProduct) setSideOpen(true)
+  },[selectedProduct])
+
+  useEffect(()=>{
+    if (!sideOpen) setSelectedProduct("")
+  },[sideOpen])
+
+
+  return (
+    <div className="product-view">
+      <div className="product-main-area">
+        <h1>Products</h1>
+        <div className="product-list">
+          {products.map(item =>
+            <ProductListItem
+              key={item.id}
+              product={item}
+              isSelected={selectedProduct === item}
+              onClick={() => setSelectedProduct(item)}
+            />
+          )}
+        </div>
+      </div>
+      <div className="product-side-panel">
+        <div className="product-side-panel-toggle-wrapper">
+          <div className="product-side-panel-toggle"
+               onClick={() => setSideOpen(!sideOpen)}>
+            {sideOpen ? '>' : '<'}
+          </div>
+        </div>
+        <ProductDetails product={selectedProduct} visible={sideOpen} />
+      </div>
+    </div>
+  );
+}
+
+export default ProductView;
